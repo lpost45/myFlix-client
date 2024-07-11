289f6a693283c379110a0e597a7ad5c97f85e238
lpost45 <lpost45@live.com>
2024-07-11 17:29:51 -0600

    Update movie-card.jsx
    added favorite movie abilities and buttons for them

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,53 @@
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom"
 
 export const MovieCard = ({ movie, onMovieClick }) => {
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user"));
+    const addFav = () => {
+        fetch("https://logan-myflix-30a490a6c5c0.herokuapp.com/users/${user.Name}/${movie._id}", {
+            "method": "POST",
+            headers: {
+                Authorization: 'Bearer ${token}',
+                "Content-Type": "application/json",
+            }
+        })
+        .then((response) => response.json())
+        .then(movies => {
+            alert("Movie added")
+        })
+        .catch(e => console.log(e))
+    }
+    const removeFav = () => {
+        fetch("https://logan-myflix-30a490a6c5c0.herokuapp.com/users/${user.Name}/${movie._id}", {
+            "method": "DELETE",
+            headers: {
+                Authorization: 'Bearer ${token}',
+                "Content-Type": "application/json",
+            }
+        })
+            .then((response) => response.json())
+            .then(movies => {
+                alert("Movie deleted")
+            })
+            .catch(e => console.log(e))
+    }
     return (
         <Card className="h-100">
             <Card.Img variant="top" src={movie.image} />
             <Card.Body>
                 <Card.Title>{movie.title}</Card.Title>
                 <Card.Text>{movie.director.Name}</Card.Text>
-                <Button onClick={() => onMovieClick(movie)} variant="link">
-                    Open
+                <Link to={'/movies/${encodeURIComponent(movie._id)}'}>
+                    <Button variant="link">Open</Button>
+                </Link>
+                <Button onClick={addFav}>
+                    Add to Favorites
+                </Button>
+                <Button onClick={removeFav}>
+                    Remove from Favorites
                 </Button>
             </Card.Body>
         </Card>
@@ -34,5 +71,4 @@ MovieCard.propTypes = {
         }),
         featured: PropTypes.bool
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
 };
